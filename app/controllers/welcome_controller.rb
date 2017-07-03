c464d7ed1a38126e5a3db49dbac9d38c0883c47c
TANYA WISE <tanya.wise@gvs.ga.gov>
2017-07-03 19:12:37 -0400

    travel app dev

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,11 @@ class WelcomeController < ApplicationController
   def index
   	@homeland = 'Panama'
   	@countries = ['Japan', 'Panama', 'Australia', 'Finland']
+  	@country_img = {}
   end
 
-  def params
+  def about_params
+  	@color = params[:color]
+  	@size = params[:shoe_size]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+   root 'welcome#index'
+  get 'index' =>'welcome#index'
 
-  get 'welcome/params'
 
-  root 'welcome#index'
-
-  get 'params' => 'welcome#params'
+  get 'params' => 'welcome#about_params'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
